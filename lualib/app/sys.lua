13b694d2904a5b60368f38acc95b5a2f520d85df
Dirk Chang <dirk@kooiot.com>
2017-06-28 14:10:02 +0800

    Implement more functions for sys

--- lualib/app/sys.lua
+++ lualib/app/sys.lua
@@ -3,25 +3,91 @@ local snax = require 'skynet.snax'
 local log = require 'utils.log'
 local class = require 'middleclass'
 local api = require 'app.api'
-local cjson = require 'cjson'
 
 local sys = class("APP_MGR_SYS")
-
-function sys:sleep(ms)
-	return skynet.sleep(ms / 10)
-end
 
 function sys:log(level, ...)
 	local f = assert(log[level])
 	return f(...)
 end
 
-function sys:fork(func)
-	skynet.fork(func)
+function sys:error(...)
+	return skynet.error(...)
+end
+
+function sys:fork(func, ...)
+	skynet.fork(func, ...)
+end
+
+function sys:timeout(ms, func)
+	return skynet.timeout(ms / 10, func)
+end
+
+function cancelable_timeout(ti, func)
+	local function cb()
+		if func then
+			func()
+		end
+	end
+	local function cancel()
+		func = nil
+	end
+	skynet.timeout(ti, cb)
+	return cancel
+end
+
+
+function sys:cancelable_timeout(ms, func)
+	local cancel = cancelable_timeout(ms / 10, dosomething)
+	return cancel
+end
+
+function sys:exit()
+	skynet.exit()
+end
+
+function sys:abort()
+	skynet.abort()
+end
+
+-- ms
+function sys:now()
+	return skynet.now() * 10
+end
+
+
+-- seconds
+function sys:time()
+	return skynet.time()
+end
+
+-- seconds
+function sys:start_time()
+	return skynet.starttime()
+end
+
+function sys:yield()
+	return skynet.yield()
+end
+
+function sys:sleep(ms)
+	return skynet.sleep(ms / 10)
 end
 
 function sys:data_api()
 	return api:new(self._app_name, self._mgr_snax)
+end
+
+function sys:self_co()
+	return coroutine.running()
+end
+
+function sys:wait()
+	return skynet.wait()
+end
+
+function sys:wakeup(co)
+	return skynet.wakeup(co)
 end
 
 function sys:app_dir()
