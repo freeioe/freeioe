bbdc2b8f2bd50c59d5746f1e70723f9827648c5a
Dirk Chang <dirk@kooiot.com>
2017-08-21 15:52:05 +0800

    modbua lua implementation

---
+++ examples/modbus_lua/app_custom_stream.lua
@@ -0,0 +1,104 @@
+local class = require 'middleclass'
+local socket = require 'skynet.socket'
+local modbus = require 'modbus.init'
+
+local app = class("XXXX_App")
+
+function app:initialize(name, conf, sys)
+	self._name = name
+	self._conf = conf
+	self._sys = sys
+	self._api = sys:data_api()
+	self._log = sys:logger()
+	self._log.debug(name.." Application initlized")
+end
+
+function app:start()
+	local stream = socket.open("127.0.0.1", 1502)
+	self._stream = stream
+	self._client1 = modbus.client({
+		send = function(msg)
+			self._dev1:dump_comm('OUT', msg)
+			socket.write(stream, msg)
+		end,
+		read = function(check, timeout)
+			timeout = os.time() + timeout
+			local buf = ""
+			local pdu = nil
+			local need_len = 5
+			while os.time() < timeout do
+				local str, err = socket.read(stream, need_len)
+				if not str then
+					return nil, err
+				end
+				self._dev1:dump_comm('IN', str)
+				buf = buf..str
+				pdu, buf, need_len = check(buf)
+				if pdu then
+					return pdu
+				end
+			end
+			return nil, "timeout"
+		end,
+	}, modbus.apdu_tcp, 1)
+
+	self._api:set_handler({
+		on_ctrl = function(...)
+			print(...)
+		end,
+	})
+	local args = {}
+	for i = 1, 10 do
+		args[#args + 1] = { 
+			name='tag'..i,
+			desc='tag'..i..' description',
+		}
+	end
+	self._dev1 = self._api:add_device("xxxx", args)
+	--self._api:del_device("xxxx")
+
+	return true
+end
+
+function app:close(reason)
+	print(self._name, reason)
+end
+
+function decode_registers(raw, count)
+	local d = modbus.decode
+	local len = d.uint8(raw, 2)
+	assert(len >= count * 2)
+	local regs = {}
+	for i = 0, count - 1 do
+		regs[#regs + 1] = d.uint16(raw, i * 2 + 3)
+	end
+	return regs
+end
+
+function app:run(tms)
+	local client = self._client1
+	if not client then
+		return
+	end
+
+	local base_address = 0x00
+	local req = {
+		func = 0x03,
+		addr = base_address,
+		len = 10,
+	}
+	local raw, err = client:request(req, 1000)
+	if not raw then 
+		self._log.error("read failed: " .. err) 
+		return
+	end
+	local regs = decode_registers(raw, 10)
+
+	for r,v in ipairs(regs) do
+		self._dev1:set_input_prop('tag'..r, "value", math.tointeger(v))
+	end
+
+	return tms
+end
+
+return app
---
+++ examples/modbus_lua/app_skynet.lua
@@ -0,0 +1,124 @@
+local class = require 'middleclass'
+local socket = require 'skynet.socket'
+local modbus = require 'modbus.init'
+local sm_client = require 'modbus.skynet_client'
+local socketchannel = require 'socketchannel'
+local serialchannel = require 'serialchannel'
+
+local app = class("XXXX_App")
+
+function app:initialize(name, conf, sys)
+	self._name = name
+	self._conf = conf
+	self._sys = sys
+	self._api = sys:data_api()
+	self._log = sys:logger()
+	self._log.debug(name.." Application initlized")
+end
+
+function app:start()
+	self._api:set_handler({
+		on_ctrl = function(...)
+			print(...)
+		end,
+	})
+
+	local sys_id = self._sys:id()
+	local config = self._conf or {
+		channel_type = 'socket'
+	}
+
+	local inputs = {}
+	for i = 1, 10 do
+		inputs[#inputs + 1] = { 
+			name='tag'..i,
+			desc='tag'..i..' description',
+		}
+	end
+
+	local dev_sn = sys_id..".modbus_"..self._name
+	local dev = self._api:add_device(dev_sn, inputs)
+	local client = nil
+
+	if config.channel_type == 'socket' then
+		opt = {
+			host = "127.0.0.1",
+			port = 1502,
+			nodelay = true,
+		}
+		print('socket')
+		client = sm_client(socketchannel, opt, modbus.apdu_tcp, i)
+	else
+		opt = {
+			port = "/dev/ttymxc1",
+			baudrate = 115200
+		}
+		print('serial')
+		client = sm_client(serialchannel, opt, modbus.apdu_rtu, i)
+	end
+	client:set_io_cb(function(io, msg)
+		dev:dump_comm(io, msg)
+	end)
+	self._client1 = client
+
+	return true
+end
+
+function app:close(reason)
+	print(self._name, reason)
+end
+
+function decode_registers(raw, count)
+	local d = modbus.decode
+	local len = d.uint8(raw, 2)
+	assert(len >= count * 2)
+	local regs = {}
+	for i = 0, count - 1 do
+		regs[#regs + 1] = d.uint16(raw, i * 2 + 3)
+	end
+	return regs
+end
+
+function app:run(tms)
+	local client = self._client1
+	if not client then
+		return
+	end
+
+	local base_address = 0x00
+	local req = {
+		func = 0x03,
+		addr = base_address,
+		len = 10,
+	}
+	local r, pdu, err = pcall(function(req, timeout) 
+		print('call request')
+		return client:request(req, timeout)
+	end, req, 1000)
+
+	if not r then 
+		pdu = tostring(pdu)
+		if string.find(pdu, 'timeout') then
+			self._log.debug(pdu, err)
+		else
+			self._log.error(pdu, err)
+		end
+		return
+	end
+
+	if not pdu then 
+		self._log.error("read failed: " .. err) 
+		return
+	end
+
+	local regs = decode_registers(raw, 10)
+	local now = self._sys:time()
+
+	for r,v in ipairs(regs) do
+		self._dev1:set_input_prop('tag'..r, "value", math.tointeger(v), now, 0)
+	end
+
+	return tms
+end
+
+return app
