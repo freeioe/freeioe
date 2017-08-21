7002e0326faebb2e117b6187999914f7b74656dd
Dirk Chang <dirk@kooiot.com>
2017-08-21 17:14:36 +0800

    default log level is error

--- snax/logger.lua
+++ snax/logger.lua
@@ -29,7 +29,7 @@ function init(...)
 	local log = require 'log'
 
 	local lname = 'skynet'
-	local max_lvl = os.getenv('IOT_LOG_LEVEL') or 'info'
+	local max_lvl = os.getenv('IOT_LOG_LEVEL') or 'error'
 
 	LOG = log.new(
 		max_lvl, -- maximum log level
