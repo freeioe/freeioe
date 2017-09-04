776050b89a14b6eaf498ab32dfd4416adb2d8cf3
Dirk Chang <dirk@kooiot.com>
2017-09-04 12:13:53 +0800

    test context

--- www/controller/index.lua
+++ www/controller/index.lua
@@ -1,5 +1,9 @@
 return {
 	get = function(self)
+		--lwf.session.data['user'] = 'aaadddddddddddddddddddddddddddddd'
+		--lwf.session:save()
+		--lwf.render('view.html', lwf.session.data)
+		--
 		--ngx.header['aaaa']= 'eee'
 		--self.route:json(ngx.req.get_headers())
 		lwf.render('view.html', self.context)
