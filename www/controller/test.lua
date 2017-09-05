bcffe270b368fa6376e384fbe885d8b959f58224
Dirk Chang <dirk@kooiot.com>
2017-09-05 15:46:38 +0800

    Test post method and reqargs

--- www/controller/test.lua
+++ www/controller/test.lua
@@ -3,5 +3,12 @@ return {
 		local get, post, files = lwf.reqargs()
 		for k,v in pairs(get) do print(k,v) end
 		lwf.json(self, get)
-	end
+	end,
+	post = function(self)
+		local get, post, files = lwf.reqargs()
+		for k,v in pairs(get) do print(k,v) end
+		print('-----------')
+		for k,v in pairs(post) do print(k,v) end
+		lwf.json(self, post)
+	end,
 }
