337e3b5dd737a2413c82a41787922501229b10bc
Dirk Chang <dirk@kooiot.com>
2017-09-25 19:57:40 +0800

    iot documents

--- www/controller/doc/iot.lua
+++ www/controller/doc/iot.lua
@@ -11,6 +11,22 @@ return {
 				title = "SocketChannel",
 				filename = "iot/socketchannel_cn.md",
 			},
+			{
+				title = "FT CSV Parser",
+				filename = "iot/ftcsv_cn.md",
+			},
+			{
+				title = "INI Parser",
+				filename = "iot/inifile_cn.md",
+			},
+			{
+				title = "Cycle Buffer",
+				filename = "iot/cyclebuffer_cn.md",
+			},
+			{
+				title = "UUID Module",
+				filename = "iot/uuid_cn.md",
+			},
 		}
 
 		lwf.render("doc.html", {doc=filename, docs=docs})
