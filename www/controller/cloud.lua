b6cf4b3d4bcaa21db7a3ee79959b334c78350a49
Dirk Chang <dirk@kooiot.com>
2017-09-30 12:26:12 +0800

    cloud control actions

--- www/controller/cloud.lua
+++ www/controller/cloud.lua
@@ -15,5 +15,31 @@ return {
 	post = function(self)
 		ngx.req.read_body()
 		local post = ngx.req.get_post_args()
+		local action = post.action
+		if action == 'enable' then
+			local option = post.option
+			local info = _("Action is accepted")
+			local cloud = snax.uniqueservice('cloud')
+			if option == 'data' then
+				cloud.post.enable_data(post.enable == 'true')
+			elseif option == 'stat' then
+				cloud.post.enable_stat(post.enable == 'true')
+			elseif option == 'log' then
+				cloud.post.enable_log(tonumber(post.enable) or 60)
+			elseif option == 'comm' then
+				cloud.post.enable_comm(tonumber(post.enable) or 60)
+			else
+				info = string.format(_("Action %s is not allowed"), option)
+			end
+			ngx.print(info)
+		end
+		if action == 'mqtt_host' then
+			local host = post.host
+			if not host or host == '' then
+				host = nil
+			end
+			dc.set('CLOUD', 'HOST', host)
+			ngx.print(_('Cloud host is changed, you need restart system to apply changes!'))
+		end
 	end
 }
