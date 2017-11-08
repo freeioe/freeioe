0cd389b17db278897ccedabaef94ceff791399d3
Dirk Chang <dirk@kooiot.com>
2017-11-08 18:01:26 +0800

    sysinfo functions

--- lualib/utils/sysinfo.lua
+++ lualib/utils/sysinfo.lua
@@ -4,12 +4,23 @@
 
 local _M = {}
 
+
+--- Get output of shell command
+--
+_M.exec = function(cmd)
+	local f = io.open(cmd)
+	if not f then
+		return nil, err
+	end
+	local s = f:read('*a')
+	f:close()
+	return s
+end
+
 --- Get the cpu model
 -- @treturn string CPU Model 
 _M.cpu_model = function()
-	local f = io.popen('cat /proc/cpuinfo')
-	local s = f:read('*a')
-	f:close()
+	local s = _M.exec('cat /proc/cpuinfo')
 	--return s:match("Hardware%s+:%s+([^%c]+)")
 	return s:match("model%s+name%s*:%s*([^%c]+)") or s:match("system%s+type%s*:%s*([^%c]+)") or 'Unknown'
 end
@@ -19,26 +30,22 @@ end
 -- @treturn string the output from uname
 _M.uname = function(arg)
 	local cmd = 'uname '..arg
-	local f = io.popen(cmd)
-	if not f then
+	local s, err = _M.exec(cmd)
+	if not s then
 		return nil, err
 	end
 
-	local s = f:read('*a')
-	f:close()
-	
 	s = string.gsub(s, "\n", "")
 
 	return s
 end
 
 local function proc_mem_info()
-	local f, err = io.popen('cat /proc/meminfo')
-	if not f then
+	local s, err = _M.exec('cat /proc/meminfo')
+	if not s then
 		return nil, err
 	end
-	local s = f:read('*a')
-	f:close()
+
 	local total = s:match("MemTotal:%s-(%d+)")
 	local free = s:match("MemFree:%s-(%d+)")
 	local used = total - free
@@ -50,12 +57,11 @@ end
 -- includes 'total' 'used' 'free'
 -- @treturn table information struct {total=xx, used=xx, free=xx}
 _M.meminfo = function()
-	local f, err = io.popen('free')
-	if not f then
+	local s, err = _M.exec('free')
+	if not s then
 		return nil, err
 	end
-	local s = f:read('*a')
-	f:close()
+
 	local info = s:gmatch("Mem:%s-(%d+)%s-(%d+)%s-(%d+)")
 	local total, used, free = info()
 	if total then
@@ -69,30 +75,28 @@ end
 -- output the loadavg as one table, includes: lavg_1, lavg_5, lavg_15, nr_running, nr_threads, last_pid
 -- @treturn table Refer to description
 _M.loadavg = function()
-   local f = io.popen('cat /proc/loadavg')
-	if not f then
+	local s, err = _M.exec('cat /proc/loadavg')
+	if not s then
 		return nil, err
 	end
-   local s = f:read('*a')
-   f:close()
 
-   -- Find the idle times in the stdout output
-   local tokens = s:gmatch('%s-([%d%.]+)')
+	-- Find the idle times in the stdout output
+	local tokens = s:gmatch('%s-([%d%.]+)')
 
-   local avgs = {}
-   for w in tokens do
-	   avgs[#avgs + 1] = w
-   end
-   local unpack = table.unpack or unpack
-   local lavg_1, lavg_5, lavg_15, nr_running, nr_threads, last_pid = unpack(avgs)
-   return {
+	local avgs = {}
+	for w in tokens do
+		avgs[#avgs + 1] = w
+	end
+	local unpack = table.unpack or unpack
+	local lavg_1, lavg_5, lavg_15, nr_running, nr_threads, last_pid = unpack(avgs)
+	return {
 		lavg_1 = lavg_1,
 		lavg_5 = lavg_5,
 		lavg_15 = lavg_15,
 		nr_running = nr_running,
 		nr_threads = nr_threads,
 		last_pid = last_pid
-   }
+	}
 end
 
 --- Get the network information
@@ -103,12 +107,11 @@ local function network_if(ifname)
 	if _VERSION == 'Lua 5.1' then
 		patt = '[^%s]'
 	end
-	local f = io.popen('LANG=C ifconfig '..ifname)
-	if not f then
+
+	local s, err = _M.exec('LANG=C ifconfig '..ifname)
+	if not s then
 		return nil, err
 	end
-	local s = f:read('*a')
-	f:close()
 
 	local hwaddr = s:match('HWaddr%s-('..patt..'+)')
 	local ipv4 = s:match('inet%s+addr:%s-('..patt..'+)')
@@ -124,12 +127,10 @@ _M.network = function()
 		patt = '[^%s:]'
 	end
 
-	local f = io.popen('cat /proc/net/dev')
-	if not f then
+	local s, err = _M.exec('cat /proc/net/dev')
+	if not s then
 		return nil, err
 	end
-	local s = f:read('*a')
-	f:close()
 
 	local tokens = s:gmatch('%s-('..patt..'+):')
 	local ifs = {}
