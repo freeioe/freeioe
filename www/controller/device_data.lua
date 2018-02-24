1b827d928586745df8373823034b9c57d75af84c
Dirk Chang <dirk@kooiot.com>
2018-02-24 11:08:18 +0800

    using vue.js for create devices page

---
+++ www/controller/device_data.lua
@@ -0,0 +1,37 @@
+local dc = require 'skynet.datacenter'
+local cjson = require 'cjson.safe'
+
+local function load_device_data()
+	local data = {}
+	local devices = dc.get('DEVICES')
+	for sn, props in pairs(devices) do
+		if props.inputs then
+			local inputs = dc.get('INPUT', sn) or {}
+			for _, input in ipairs(props.inputs) do
+				input.value = inputs[input.name] and inputs[input.name].value or {}
+			end
+		end
+		if props.outputs then
+			local outputs = dc.get('OUTPUT', sn) or {}
+			for _, output in ipairs(props.outputs) do
+				output.value = outputs[output.name] and outputs[output.name].value or {}
+			end
+		end
+		data[#data + 1] = {
+			sn = sn,
+			props = props
+		}
+	end
+	return data
+end
+
+return {
+	get = function(self)
+		if lwf.auth.user == 'Guest' then
+			return
+		end
+		local data = load_device_data()
+		ngx.header.content_type = "application/json; charset=utf-8"
+		ngx.print(cjson.encode({devices=data}))
+	end
+}
