9c9c413c69116569852731485032e42dd5999781
Dirk Chang <dirk@kooiot.com>
2018-03-16 15:09:18 +0800

    improve batch runner service

--- service/run_batch.lua
+++ service/run_batch.lua
@@ -5,10 +5,19 @@ local log = require 'utils.log'
 
 local arg = table.pack(...)
 assert(arg.n <= 2)
-local id = arg[1]
+local batch_id = arg[1]
+
+local tasks = {}
+
+local function gen_task_id()
+	local id = batch_id .. '.' .. #tasks
+	tasks[#tasks + 1] = id
+	return id
+end
 
 local batch_env = {
 	INSTALL_APP = function(inst, name, version, sn, conf)
+		local id = gen_task_id()
 		assert(inst and name and version)
 		local r, err = skynet.call("UPGRADER", "lua", "install_app", id, {
 			name = name,
@@ -19,11 +28,13 @@ local batch_env = {
 		})
 	end,
 	REMOVE_APP = function(inst)
+		local id = gen_task_id()
 		local r, err = skynet.call("UPGRADER", "lua", "uninstall_app", id, {
 			inst = inst,
 		})
 	end,
 	UPGRADE_APP = function(inst, version, sn, conf)
+		local id = gen_task_id()
 		local r, err = skynet.call("UPGRADER", "lua", "upgrade_app", id, {
 			inst = inst,
 			version = version,
@@ -32,6 +43,7 @@ local batch_env = {
 		})
 	end,
 	UPGRADE_SYS = function(version, skynet_version, need_ack)
+		local id = gen_task_id()
 		assert(version)
 		local no_ack = need_ack and true or false
 		local skynet_args = skynet_version and {version=skynet_version} or nil
@@ -42,25 +54,28 @@ local batch_env = {
 		})
 	end,
 	TEST = function(...)
+		local id = gen_task_id()
 		print(...)
 		log.debug(...)
 	end,
 }
 
 skynet.start(function()
-	log.notice("Batch script: ", id)
-	local script = arg.n == 2 and arg[2] or datacenter.get("BATCH", id, "script")
+	log.notice("Batch script: ", batch_id)
+	local script = arg.n == 2 and arg[2] or datacenter.get("BATCH", batch_id, "script")
 	assert(script)
 	local f, err = load(script, nil, "bt", batch_env)
 	if not f then
 		log.error("Loading batch script failed", err)
 	else
 		local r, err = xpcall(f, debug.traceback)
+		print(cjson.encode(tasks))
 		if not r then
-			datacenter.set("BATCH_RESULT", id, err)
+			log.warning("BATCH run error", err)
+			datacenter.set("BATCH_RESULT", batch_id, err)
 		else
-			datacenter.set("BATCH_RESULT", id, "DONE")
+			datacenter.set("BATCH_RESULT", batch_id, "DONE")
 		end
-		datacenter.set("BATCH", id, nil)
+		datacenter.set("BATCH", batch_id, nil)
 	end
 end)
