e54ca49de7f2abe96a10d405903588e5a23d8169
Dirk Chang <dirk@kooiot.com>
2018-03-19 09:44:13 +0800

    finished run batch service

--- service/run_batch.lua
+++ service/run_batch.lua
@@ -1,4 +1,5 @@
 local skynet = require "skynet"
+local snax = require "skynet.snax"
 local datacenter = require "skynet.datacenter"
 local cjson = require "cjson.safe"
 local log = require 'utils.log'
@@ -9,15 +10,19 @@ local batch_id = arg[1]
 
 local tasks = {}
 
-local function gen_task_id()
+local function gen_task_id(cate, info)
 	local id = batch_id .. '.' .. #tasks
-	tasks[#tasks + 1] = id
+	tasks[#tasks + 1] = {
+		id = id,
+		cate = cate or 'app',
+		info = info,
+	}
 	return id
 end
 
 local batch_env = {
 	INSTALL_APP = function(inst, name, version, sn, conf)
-		local id = gen_task_id()
+		local id = gen_task_id('app', "Install application "..name.." as "..inst)
 		assert(inst and name and version)
 		local r, err = skynet.call("UPGRADER", "lua", "install_app", id, {
 			name = name,
@@ -28,13 +33,13 @@ local batch_env = {
 		})
 	end,
 	REMOVE_APP = function(inst)
-		local id = gen_task_id()
+		local id = gen_task_id('app', "Remove application instance "..inst)
 		local r, err = skynet.call("UPGRADER", "lua", "uninstall_app", id, {
 			inst = inst,
 		})
 	end,
 	UPGRADE_APP = function(inst, version, sn, conf)
-		local id = gen_task_id()
+		local id = gen_task_id('app', "Upgrade application instance "..inst.." to version "..version)
 		local r, err = skynet.call("UPGRADER", "lua", "upgrade_app", id, {
 			inst = inst,
 			version = version,
@@ -43,7 +48,7 @@ local batch_env = {
 		})
 	end,
 	UPGRADE_SYS = function(version, skynet_version, need_ack)
-		local id = gen_task_id()
+		local id = gen_task_id('sys', "Upgrade system to version "..version.." "..(skynet_version and ("with skynet "..skynet_version) or "withou skynet"))
 		assert(version)
 		local no_ack = need_ack and true or false
 		local skynet_args = skynet_version and {version=skynet_version} or nil
@@ -69,6 +74,7 @@ skynet.start(function()
 
 	--- Loading script string
 	local f, err = load(script, nil, "bt", batch_env)
+	local cloud = snax.uniqueservice('cloud')
 	if not f then
 		log.error("Loading batch script failed", err)
 		cloud.post.action_result("batch_script", batch_id, false, err)
