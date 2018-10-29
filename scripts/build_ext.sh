a3c07a6fe582c6fd51315b88b41ad2ad595975bb
Dirk Chang <dirk@kooiot.com>
2018-10-29 18:44:44 +0800

    shell script for releasing extension

---
+++ scripts/build_ext.sh
@@ -0,0 +1,32 @@
+
+if [ $# -lt 3 ] ; then
+	echo "Usage: build_ext.sh <cpu arch> <toolchain script name> <target dir>"
+	exit 0
+fi
+
+PLAT_ARCH=$1
+TOOLCHAIN=$2
+TARGET_DIR=$3
+
+rm build -rf
+
+printf "ARCH: $PLAT_ARCH \t TOOLCHAINE: $TOOLCHAIN \t TARGET: $TARGET_DIR \n"
+
+if [ "$TOOLCHAIN" == "native" ]; then
+	premake5 gmake
+else
+	echo "export toolchain"
+	premake5 --file=premake5_openwrt.lua gmake
+	. ~/toolchains/$TOOLCHAIN
+fi
+
+echo $CC
+cd build
+make config=release
+
+cd ..
+file bin/Release/*.so
+cp bin/Release/*.so $TARGET_DIR
+
+echo "===============================DONE=================================="
+
---
+++ scripts/build_ext_all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+
+declare -A plats
+
+plats["linux/x86_64"]="native"
+plats["openwrt/arm_cortex-a9_neon"]="imx6_exports.sh"
+plats["openwrt/mips_24kc"]="mips_24kc.sh"
+plats["openwrt/x86_64"]="x86_64_glibc.sh"
+plats["openwrt/aarch64_cortex-a53"]="bp3plus_exports.sh"
+
+for item in "${!plats[@]}"; 
+do
+	bash $SCRIPTPATH/build_ext.sh $item ${plats[$item]} ~/mycode/freeioe_prebuild_exts/$item/
+done
