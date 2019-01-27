ef57da29e13739137b6d217e6e44f0953fb4067c
Dirk Chang <dirk@kooiot.com>
2019-01-27 15:46:59 +0800

    process-monitor will be built with openwrt

--- scripts/build_ext_all.sh
+++ scripts/build_ext_all.sh
@@ -16,6 +16,7 @@ plats["openwrt/arm_cortex-a9_neon"]="imx6_exports.sh"
 plats["openwrt/mips_24kc"]="mips_24kc.sh"
 plats["openwrt/x86_64"]="x86_64_glibc.sh"
 plats["openwrt/aarch64_cortex-a53"]="bp3plus_exports.sh"
+plats["openwrt/arm_cortex-a7_neon-vfpv4"]="sunxi_a7.sh"
 
 for item in "${!plats[@]}"; 
 do
