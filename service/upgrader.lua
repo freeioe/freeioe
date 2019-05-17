6c94f606e50eec95ab1b1eaf66bda7142ccc17dd
Dirk Chang <dirk@kooiot.com>
2019-05-17 15:00:38 +0800

    avoid empty string instance name

--- service/upgrader.lua
+++ service/upgrader.lua
@@ -202,6 +202,9 @@ function command.install_app(id, args)
 	if beta and not ioe.beta() then
 		return false, "Device is not in beta mode! Cannot install beta version"
 	end
+	if string.len(inst_name or '') == 0 then
+		return false, "Application instance cannot be am empty string"
+	end
 
 	local sn = args.sn or gen_app_sn(inst_name)
 	local conf = args.conf or {}
