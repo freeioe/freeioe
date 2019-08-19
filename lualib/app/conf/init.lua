d3998b24ec4d9c873c7ed9f771c0eee7541395ef
Dirk Chang <dirk@kooiot.com>
2019-08-19 13:34:37 +0800

    remove conf in gitignore

---
+++ lualib/app/conf/init.lua
@@ -0,0 +1,53 @@
+local skynet = require 'skynet'
+local cjson = require 'cjson.safe'
+local class = require 'middleclass'
+
+local conf = class("FREEIOE_APP_CONF_INIT")
+
+local function load_defaults_real(filename)
+	local f = assert(io.open(filename))
+	local str = f:read('*a')
+	local data = assert(cjson.decode(str))
+	f:close()
+
+	--- parse the json data
+	
+	local default = {}
+	for _, v in ipairs(data) do
+		if v.default ~= nil then
+			default[v.name] = v.default
+		end
+	end
+
+	return default
+end
+
+local function load_defaults(filename)
+	local r, data = pcall(load_defaults_real, filename)
+	if not r then
+		--skynet.error('Load app config file '..filename..' failed!', data)
+		return {}
+	else
+		skynet.error('Loaded app config template file!!')
+	end
+	return data or {}
+end
+
+function conf:initialize(sys, conf_json)
+	self._sys = sys
+	self._conf_json = conf_json or 'conf.json'
+
+	local filename = self._sys:app_dir()..'/'..self._conf_json
+	self._default = load_defaults(filename)
+
+end
+
+function conf:map(conf_t)
+	return setmetatable(conf_t or {}, {__index = self._default})
+end
+
+function conf:__call(conf_t)
+	return self:map(conf_t)
+end
+
+return conf
