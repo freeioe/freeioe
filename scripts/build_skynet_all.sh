9a494206caeed380910bca797826621459d2ad34
Dirk Chang <dirk@kooiot.com>
2020-07-08 11:37:33 +0800

    support mdm9607 and sunxi H6 hardwares

--- scripts/build_skynet_all.sh
+++ scripts/build_skynet_all.sh
@@ -13,10 +13,12 @@ declare -A plats
 plats["linux/x86_64"]="native"
 #plats["openwrt/arm_cortex-a9_neon"]="imx6_exports.sh"
 plats["openwrt/17.01/arm_cortex-a9_neon"]="tg3030_exports.sh"
+plats["openwrt/17.01/arm_cortex-a7_neon"]="mdm9607_exports.sh"
 plats["openwrt/18.06/mips_24kc"]="mips_24kc.sh"
 plats["openwrt/18.06/x86_64"]="x86_64_glibc.sh"
 #plats["openwrt/aarch64_cortex-a53"]="bp3plus_exports.sh"
 plats["openwrt/19.07/arm_cortex-a7_neon-vfpv4"]="sunxi_a7.sh"
+plats["openwrt/19.07/aarch64_cortex-a53"]="sunxi_a53.sh"
 plats["openwrt/19.07/x86_64"]="x86_64_glibc_19.07.sh"
 # plats["android/arm"]="android_arm.sh"
 
