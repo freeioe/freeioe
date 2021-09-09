9624e850f3a6bc1e122cc84d3bcb5bc409967ab0
Dirk Chang <dirk@kooiot.com>
2021-09-09 13:01:39 +0800

    fixed upgrade issue

--- service/upgrader.lua
+++ service/upgrader.lua
@@ -499,8 +499,13 @@ function command.user_access(auth_code)
 end
 
 local function download_upgrade_skynet(id, args, cb)
+	local skynet_version = args
+	if type(args) == 'table' then
+		skynet_version = args.version
+	end
+
 	--local is_windows = package.config:sub(1,1) == '\\'
-	local version, beta = parse_version_string(args.version)
+	local version, beta = parse_version_string(skynet_version)
 	return create_sys_download('skynet', version, cb, ".tar.gz", args.token, true)
 end
 
@@ -733,20 +738,22 @@ function command.upgrade_core(id, args)
 		end
 	end
 
-	local skynet_version = args.skynet
+
 	--- Upgrade skynet only
 	if not args.version or tonumber(args.version) <= 0 or string.lower(args.version) == 'none' then
-		return download_upgrade_skynet(id, skynet_version, function(path)
+		return download_upgrade_skynet(id, args.skynet, function(path)
 			return start_upgrade_proc(nil, path)
 		end)
 	end
 
+	-- Upgrade both
 	local version, beta = parse_version_string(args.version)
+	local skynet_version = type(args.skynet) == 'table' and args.skynet.version or args.skynet
 
 	return create_sys_download('freeioe', version, function(path)
 		local freeioe_path = path
-		if skynet_version and tonumber(skynet_version) > 0 then
-			return download_upgrade_skynet(id, skynet_version, function(path)
+		if skynet_version then
+			return download_upgrade_skynet(id, args.skynet, function(path)
 				return start_upgrade_proc(freeioe_path, path)
 			end)
 		else
