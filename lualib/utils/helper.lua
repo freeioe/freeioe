0d487bd18342af6bc0097f7cd7733e6a273985ce
Dirk Chang <dirk@kooiot.com>
2026-01-06 11:15:37 +0800

    fixed k in loop is const in lua5.5

--- lualib/utils/helper.lua
+++ lualib/utils/helper.lua
@@ -22,11 +22,14 @@ function _M.encode_query_string (t, sep)
 	local buf = { }
 	for k, v in pairs(t) do
 		if type(k) == "number" and type(v) == "table" then
-			k, v = v[1], v[2]
+			buf[i + 1] = escape(v[1])
+			buf[i + 2] = "="
+			buf[i + 3] = escape(tostring(v[2]))
+		else
+			buf[i + 1] = escape(k)
+			buf[i + 2] = "="
+			buf[i + 3] = escape(tostring(v))
 		end
-		buf[i + 1] = escape(k)
-		buf[i + 2] = "="
-		buf[i + 3] = escape(tostring(v))
 		buf[i + 4] = sep
 		i = i + 4
 	end
